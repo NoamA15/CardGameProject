63d9ecac7b58a6ef7e5303a08ce459717ac5a584
NoamA15 <noam15982@gmail.com>
2023-01-12 10:53:17 +0200

    Card

--- CardGame/Card.py
+++ CardGame/Card.py
@@ -1,9 +1,34 @@
 class Card:
-    def __init__(self):
-        pass
+    def __init__(self, value, suit):
+        if type(value) != int:
+            raise TypeError("Argument value must be of type int")
+        if value < 1:
+            raise ValueError("Argument value must be > 1")
+        if value > 13:
+            raise ValueError("Argument value must be < 13")
+        if type(suit) != str:
+            raise TypeError("Argument suit must be of type str")
+        if suit not in ["Diamond", "Spade", "Heart", "Club"]:
+            raise ValueError("Argument suit must be a valid suit")
+        self.value = value
+        self.suit = suit
 
     def __gt__(self,other):
-        pass
+        if type(other) != Card:
+            raise TypeError("Argument other must be of type Card")
+        if self.value == 1 and other.value != 1:
+            return True
+        if other.value == 1 and self.value != 1:
+            return False
+        if self.value > other.value:
+            return True
+        elif self.value == other.value:
+            list1 = ["Diamond", "Spade", "Heart", "Club"]
+            return list1.index(self.suit) > list1.index(other.suit)
+        else:
+            return False
 
     def __eq__(self, other):
-        pass
+        if type(other) != Card:
+            raise TypeError("Argument other must be of type Card")
+        return self.value == other.value
